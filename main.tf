7f0932ac6c00a7623e82ada32fb951e9ca6a2ae6
Chetan Chandane <chetanchandane10@gmail.com>
2025-02-12 11:31:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 }
 
 # Local variables block for configuration values
-# locals {
-#     aws_key = "My_AWS_key"   # SSH key pair name for EC2 instance access
-# }
+ locals {
+     aws_key = "My_AWS_key"   # SSH key pair name for EC2 instance access
+}
 
 # Security group to allow HTTP, HTTPS, and SSH traffic
 resource "aws_security_group" "wordpress_sg" {
@@ -54,7 +54,6 @@ resource "aws_security_group" "wordpress_sg" {
 resource "aws_instance" "my_server" {
    ami           = data.aws_ami.amazonlinux.id  # Use the AMI ID from the data source
    instance_type = var.instance_type            # Use the instance type from variables
-   key_name      = "${var.aws_key}"          # Specify the SSH key pair name
    vpc_security_group_ids = [aws_security_group.wordpress_sg.id]  # Attach the security group
    user_data = filebase64("wp_install.sh")
    # Add tags to the EC2 instance for identification
